1b6fedc970ecc8a08fb3f56eae781159b0be2f49
david-vizena <davidvizena@gmail.com>
2025-12-02 16:45:57 -0600

    Add authentication UI: sign-in page, UserButton component, and user actions

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -7,11 +7,11 @@ const Header = () => {
 	return (
 		<header className='w-full border-b'>
 			<div className='wrapper flex-between'>
-				<div className='flext-start'>
-					<Link href='/' className='flex-start'>
+				<div className='flex-start'>
+					<Link href='/' className='flex-start ml-4'>
 						<Image
 							src='/images/logo.svg'
-							alt={`{$APP_NAME} logo`}
+							alt={`${APP_NAME} logo`}
 							height={48}
 							width={48}
 							priority={true}
@@ -21,6 +21,7 @@ const Header = () => {
 						</span>
 					</Link>
 				</div>
+				<div className='hidden md:block'></div>
 				<Menu />
 			</div>
 		</header>
