789982b967f841792bc6dab3b207f4aa1c2c2da4
david-vizena <davidvizena@gmail.com>
2026-01-02 15:12:41 -0600

    feat: add protected paths authentication check in middleware

--- auth.ts
+++ auth.ts
@@ -121,6 +121,23 @@ export const config = {
 			return token;
 		},
 		authorized({ request, auth }: any) {
+			// Array of regex patters of paths to protect
+			const protectedPaths = [
+				/\/shipping-address/,
+				/\/payment-method/,
+				/\/place-order/,
+				/\/profile/,
+				/\/user\/(.*)/,
+				/\/order\/(.*)/,
+				/\/admin/,
+			];
+
+			// Get pathname form the req URL object
+			const { pathname } = request.nextUrl;
+
+			// Check if user is not authenticated and accessing a protected path
+			if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
 			// Check for session cart cookie
 			if (!request.cookies.get('sessionCartId')) {
 				// Generate new session cart id cookie
