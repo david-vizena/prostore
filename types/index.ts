858facda0888b083de75fd437d33f230c9f79915
david-vizena <davidvizena@gmail.com>
2026-01-02 14:25:38 -0600

    feat: enable updateUserAddress import and complete shipping address form

--- types/index.ts
+++ types/index.ts
@@ -16,7 +16,7 @@ export type Product = z.infer<typeof insertProductSchema> & {
 
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
-export type shippingAddressSchema = z.infer<typeof shippingAddressSchema>;
+export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
 
 // Extend NextAuth types to include role
 declare module 'next-auth' {
